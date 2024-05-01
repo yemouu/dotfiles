a9f26e180f200e02a49d1f49e3ea9ab478f714e8
yemou <dev@lilac.pink>
2024-05-01 01:58:43 -0400

    Remove configs for home server.
    These configs can now be found in their own repo alongside configs
    for other servers I may manage in the future.

--- flake.nix
+++ flake.nix
@@ -5,43 +5,16 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    sops-nix.url = "github:Mic92/sops-nix";
     yemou-scripts = {
       url = "gitlab:yemou/scripts";
       # url = "path:/home/mou/misc/repos/scripts";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    esquid = {
-      url = "github:Things-N-Stuff/eSquid";
-      # url = "path:/home/mou/misc/repos/eSquid";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nix-minecraft = {
-      url = "github:Infinidoge/nix-minecraft";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs = { self, nixpkgs, sops-nix, home-manager, ... }@attrs: {
     nixosConfigurations = {
-      # lily = nixpkgs.lib.nixosSystem {
-      #   system = "x86_64-linux";
-      #   specialArgs = attrs;
-      #   modules = [
-      #     ./lily/config.nix
-      #     home-manager.nixosModules.home-manager
-      #     {
-      #       home-manager.extraSpecialArgs = attrs;
-      #       home-manager.useGlobalPkgs = true;
-      #       home-manager.useUserPackages = true;
-      #       home-manager.users.mou = import ./lily/home.nix;
-      #     }
-      #   ];
-      # };
-
       lutea = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = attrs;
